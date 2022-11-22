9069307192389bd75c19787adadbdbcbebb7d9c2
dev-lincoln <lincoln.dev@outlook.com>
2022-11-22 15:01:20 -0300

    Prerando deploy no Render

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const funcionarioRouter = require("./routes/funcionarios");
 const clienteRouter = require("./routes/clientes");
